6f4a1f734ae48696ef84c457ce58240d36d7446e
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2017-10-20 23:56:43 -0200

    Adding new SimpleRecyclerView sample

---
+++ sample/src/main/java/br/com/mobiplus/simplerecyclerview/sample/SimpleRecyclerViewActivity.java
@@ -0,0 +1,42 @@
+package br.com.mobiplus.simplerecyclerview.sample;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.List;
+
+import br.com.mobiplus.simplerecyclerview.sample.model.Stadium;
+import br.com.mobiplus.simplerecylerview.SimpleLinearRecyclerView;
+import br.com.mobiplus.simplerecylerview.adapter.OnItemClickListener;
+
+public class SimpleRecyclerViewActivity extends AppCompatActivity implements MockService.DataCallback<List<Stadium>> {
+
+    private MockService mockService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_recycler_view);
+
+        mockService = new MockService(getApplicationContext());
+
+        this.loadData();
+    }
+
+    private void loadData() {
+        mockService.loadStadiumList(this);
+    }
+
+    @Override
+    public void onLoadData(List<Stadium> stadiumList) {
+        SimpleLinearRecyclerView recyclerView = (SimpleLinearRecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setCollection(stadiumList, new OnItemClickListener<Stadium>() {
+            @Override
+            public void onItemClick(Stadium stadium, @IdRes int resId) {
+                Toast.makeText(SimpleRecyclerViewActivity.this, "Clicked item: " + stadium.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
